89b0305b476b0be1d3c1608a5c66e82ad1d64990
Chase Van Halen <cvanhalen@perscholas.org>
2023-02-02 10:59:47 -0600

    updated code

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,6 @@ const axios = require('axios')
 const app = express();
 
 
-console.log(axios);
-
-
 
 app.listen(5000, () => {
     console.log(`Server is Listening on 5000`);
---
+++ client/src/components/card/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Image from '../image'
+import CardBody from '../card_body'
+import Button from '../button'
+
+const Card = () => {
+  return (
+    <div>
+        <Image />
+        <CardBody />
+        <Button />
+    </div>
+  )
+}
+
+export default Card
