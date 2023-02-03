f425d895104c4aaa3889433741fa1d5e8e41875c
Danielle <danielle@kendallspace.net>
2023-02-03 09:49:52 -0500

    clean up styles and js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,7 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
 
-
-
 app.listen(5000, () => {
-    console.log(`Server is Listening on 5000`);
-})
+  console.log(`Server is Listening on port 5000`);
+});
